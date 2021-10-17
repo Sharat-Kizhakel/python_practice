4f6e0bbbac7697d44fb13cd617fe55fce5b95557
Sharat-Kizhakel <sharatprasad.cs19@bmsce.ac.in>
2021-10-17 18:28:13 +0530

    Added AI need to work on minimax error

--- LAB1/tic_tac_toe.py
+++ LAB1/tic_tac_toe.py
@@ -13,6 +13,7 @@
 # generating a board
 import random
 import sys
+import types
 global count
 
 global active_player
@@ -39,9 +40,10 @@ def new_board():
 
 def new_game():
     global board
-    board = ["-", "-", "-",
-             "-", "-", "-",
-             "-", "-", "-"]
+    # board = ["-", "-", "-",
+    #          "-", "-", "-",
+    #          "-", "-", "-"]
+    board = ['-' for x in range(9)]
     continue_game = True
     new_board()
     print("WELCOME TO TIC TAC TOE:\n")
@@ -51,17 +53,21 @@ def new_game():
 
         active_player = "X" if c == 1 else switch_player(active_player)
         player_move(active_player)
+        global outcome
         outcome = game_outcome()
-        if outcome == True:
-            print("We have a Winner:")
-            print(active_player)
 
-            board.clear()
+        if type(outcome) is tuple:
 
-            if input("Do you want to play again:y/n?").lower() == 'y':
-                new_game()
-            else:
-                sys.exit()
+            if outcome[0] == True:
+                print("We have a Winner:")
+                print(
+                    "Computer Won:" + outcome[1]) if outcome[1] == "X"else print("User Won:" + outcome[1])
+                board.clear()
+
+                if input("Do you want to play again:y/n?").lower() == 'y':
+                    new_game()
+                else:
+                    sys.exit()
         elif outcome == "Draw":
             print("Draw")
             board.clear()
@@ -88,9 +94,68 @@ def switch_player(current):
 
 
 def comp_move():  # assuming computer always plays X
-    available_list = [index for index, i in enumerate(board) if i == '-']
-    comp_choice = random.choice(available_list)
-    return comp_choice + 1
+    # available_list = [index for index, i in enumerate(board) if i == '-']
+    # comp_choice = random.choice(available_list)
+    # return comp_choice + 1
+
+    bestScore = -1000
+    global maxMove
+    maxMove = 0
+
+    for index, i in enumerate(board):
+        if i == '-':
+            board[index] = 'X'  # best move this maximizing
+            # minimizing right after maximizing in game Tree hence False
+            score = minimax(board, 0, False)
+
+            # once score is calculated reverting back to original board
+            board[index] = '-'
+            if score > bestScore:
+                bestScore = score
+                maxMove = index
+                print(maxMove)
+                print(bestScore)
+
+    return maxMove + 1
+
+
+def minimax(board, depth, isMaximizing):
+    # defining the terminal states for the recursion
+    global maxScore, minScore
+    global check_for_win
+    check_for_win = game_outcome()
+
+    if type(check_for_win) is tuple:
+        if check_for_win[0] == True:
+            if check_for_win[1] == 'X':
+                return 1
+            elif check_for_win[1] == 'O':
+                return -1
+    elif check_for_win == "Draw":
+        return 0
+    if isMaximizing:
+        maxScore = -1000
+        for index, i in enumerate(board):
+            if i == '-':
+                board[index] = 'X'  # best move
+                score = minimax(board, 0, False)
+                # once score is calculated reverting back to original board
+                board[index] = '-'
+                if score > maxScore:
+                    maxScore = score
+
+        return maxScore
+    else:  # mock trial b/w human and AI: human is min. and AI is max function
+        minScore = 1000
+        for index, i in enumerate(board):
+            if i == '-':
+                board[index] = 'O'
+                # Once minimized we again maximize hence True
+                score = minimax(board, 0, True)
+                board[index] = '-'
+                if score < minScore:
+                    minScore = score
+        return minScore
 
 
 def check_if_occupied(userchoice):
@@ -127,15 +192,26 @@ def player_move(current):
             board[user_choice - 1] = "O"
             new_board()
     else:
-        print(valid_move)
+
         print("Not a valid move. Its already occupied. Try again")
         player_move(current)
 
 
 def game_outcome():
     # if any check returns True then its false
-    if (check_row() or check_col() or check_diag()):
-        return True
+    cr = check_row()
+    cl = check_col()
+    cd = check_diag()
+    if type(cr) is tuple:
+        if (cr[0]):
+            return True, cr[1]
+    elif type(cl) is tuple:
+        if (cl[0]):
+            return True, cl[1]
+    elif type(cd) is tuple:
+
+        if (cd[0]):
+            return True, cd[1]
     elif (all_filled()):
         return "Draw"
     else:
@@ -165,7 +241,7 @@ def check_row():
             row.append(board[i])
 
         if(row == ["X", "X", "X"] or row == ["O", "O", "O"]):
-            return True
+            return True, row[0]
         else:
             row.clear()
             c1 = c2
@@ -189,7 +265,7 @@ def check_col():
             col.append(board[i])
 
         if(col == ["X", "X", "X"] or col == ["O", "O", "O"]):
-            return True
+            return True, col[0]
         else:
             col.clear()
             c1 += 1
@@ -213,7 +289,7 @@ def check_diag():
         diag.append(board[i])
 
         if(diag == ["X", "X", "X"] or diag == ["O", "O", "O"]):
-            return True
+            return True, diag[0]
     c1 = 2
     c2 = 7
     for i in range(c1, c2, 2):  # other diagoanal
@@ -221,7 +297,9 @@ def check_diag():
         diag1.append(board[i])
 
         if(diag1 == ["X", "X", "X"] or diag1 == ["O", "O", "O"]):
-            return True
+            return True, diag1[0]
+    diag.clear()
+    diag1.clear()
     return False
 
 
